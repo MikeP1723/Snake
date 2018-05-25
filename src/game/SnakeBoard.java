c0dfdbc567680fa11605d87cf0a50ee933586828
Christopher M Peterson <MikeP1723@gmail.com>
2018-05-24 22:40:07 -0400

    Cleaned up imports

--- src/game/SnakeBoard.java
+++ src/game/SnakeBoard.java
@@ -1,25 +1,12 @@
 package game;
 
-import java.awt.Button;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.io.File;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.Timer;
 
 public class SnakeBoard extends JPanel implements ActionListener {
 	
